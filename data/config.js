6419e5f5b99e742ad2f95146405a99504127601e
marymaryranting-collab <marymaryranting@gmail.com>
2026-07-02 13:43:49 +0200

    Create config.js

---
+++ data/config.js
@@ -0,0 +1,18 @@
+/*
+=========================================
+YURI'S LIBRARY
+Configuration
+=========================================
+*/
+
+const GAME = {
+
+    title: "Yuri's Library",
+
+    version: "1.0.0",
+
+    author: "Mary",
+
+    maxAffection: Infinity
+
+};
