4d4b69bc92fb854778f28f1118b27594f64f33ea
marymaryranting-collab <marymaryranting@gmail.com>
2026-07-02 13:35:56 +0200

    Create dialogue.js

---
+++ data/dialogue.js
@@ -0,0 +1,145 @@
+/*
+==================================
+YURI'S LIBRARY
+Dialogue Database
+==================================
+*/
+
+const dialogues = [
+
+{
+    id: "welcome",
+
+    category: "general",
+
+    affectionRequired: 0,
+
+    affectionReward: 1,
+
+    repeatable: true,
+
+    text:
+    "Welcome... I was hoping you would visit today."
+},
+
+{
+    id: "books1",
+
+    category: "books",
+
+    affectionRequired: 0,
+
+    affectionReward: 1,
+
+    repeatable: true,
+
+    text:
+    "I've been rereading one of my favorite novels. Somehow every time I read it, I discover something new."
+},
+
+{
+    id: "tea1",
+
+    category: "tea",
+
+    affectionRequired: 0,
+
+    affectionReward: 1,
+
+    repeatable: true,
+
+    text:
+    "A warm cup of tea can make even the quietest afternoon feel comforting."
+},
+
+{
+    id: "poetry1",
+
+    category: "poetry",
+
+    affectionRequired: 3,
+
+    affectionReward: 2,
+
+    repeatable: true,
+
+    text:
+    "Sometimes poetry says things that ordinary words simply can't."
+},
+
+{
+    id: "library1",
+
+    category: "library",
+
+    affectionRequired: 5,
+
+    affectionReward: 2,
+
+    repeatable: true,
+
+    text:
+    "I like to imagine this library growing every time you visit."
+},
+
+{
+    id: "friend1",
+
+    category: "friendship",
+
+    affectionRequired: 8,
+
+    affectionReward: 2,
+
+    repeatable: true,
+
+    text:
+    "It's... nice having someone who doesn't mind spending quiet moments together."
+},
+
+{
+    id: "rain1",
+
+    category: "rain",
+
+    affectionRequired: 10,
+
+    affectionReward: 3,
+
+    repeatable: true,
+
+    text:
+    "Rain always makes the world feel a little softer... don't you think?"
+},
+
+{
+    id: "books2",
+
+    category: "books",
+
+    affectionRequired: 15,
+
+    affectionReward: 3,
+
+    repeatable: true,
+
+    text:
+    "If I recommend too many books... you'll still come back and tell me what you thought... right?"
+},
+
+{
+    id: "smile",
+
+    category: "general",
+
+    affectionRequired: 20,
+
+    affectionReward: 3,
+
+    repeatable: true,
+
+    text:
+    "...I'm smiling a little more lately. I think I know why."
+}
+
+];
