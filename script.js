8c465ed14203bf2a2f20506ff5cb0f713a7615d8
marymaryranting-collab <marymaryranting@gmail.com>
2026-07-02 13:33:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,139 @@
+/*
+==================================
+YURI'S LIBRARY
+Main Script
+Version 1.0
+==================================
+*/
 
+const dialogueElement = document.getElementById("dialogue");
+const affectionElement = document.getElementById("affection");
+const visitsElement = document.getElementById("visits");
+
+const talkButton = document.getElementById("talkButton");
+
+
+// --------------------
+// Default Save
+// --------------------
+
+let saveData = {
+
+    affection: 0,
+
+    visits: 1,
+
+    seenDialogue: []
+
+};
+
+
+// --------------------
+// Load Save
+// --------------------
+
+function loadGame(){
+
+    const data = localStorage.getItem("yuriLibrarySave");
+
+    if(data){
+
+        saveData = JSON.parse(data);
+
+    }
+
+}
+
+
+// --------------------
+// Save Game
+// --------------------
+
+function saveGame(){
+
+    localStorage.setItem(
+
+        "yuriLibrarySave",
+
+        JSON.stringify(saveData)
+
+    );
+
+}
+
+
+// --------------------
+// Update UI
+// --------------------
+
+function updateUI(){
+
+    affectionElement.textContent = saveData.affection;
+
+    visitsElement.textContent = saveData.visits;
+
+}
+
+
+// --------------------
+// First Visit
+// --------------------
+
+function initializeGame(){
+
+    loadGame();
+
+    saveData.visits++;
+
+    updateUI();
+
+    saveGame();
+
+}
+
+
+// --------------------
+// Talk
+// --------------------
+
+function talk(){
+
+    const dialogue = getDialogue();
+
+    dialogueElement.classList.remove("fade");
+
+    void dialogueElement.offsetWidth;
+
+    dialogueElement.classList.add("fade");
+
+    dialogueElement.textContent = dialogue.text;
+
+    if(dialogue.affection > 0){
+
+        addAffection(dialogue.affection);
+
+    }
+
+    saveGame();
+
+}
+
+
+// --------------------
+// Buttons
+// --------------------
+
+talkButton.addEventListener(
+
+    "click",
+
+    talk
+
+);
+
+
+// --------------------
+// Start
+// --------------------
+
+initializeGame();
